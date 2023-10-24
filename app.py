8a78e239252e50feca334aae9be77cd611b170c4
DevDiscoverer <137151829+DevDiscoverer@users.noreply.github.com>
2023-10-24 21:12:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+# print("I am DevDiscoverer")
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+
+
+# Variables are used to temporarly store data in computer.
+# price = 10
+# print(price)
+# here price is a identifier and 10 is the value that it stores.
+
+# price = 20 
+# print(price)
+#python is executed line by line so if you comment oyt 10 then it prints 20
+
+# is_published = True
+# Boolean to save yes or no 
+
+# patient_name = "John Smith"
+# patient_age = 20
+# is_new = True
+
+# name = input("What is your Name ? ")
+# print ("Hi" + name)
+
+# myName = input("Plz let me know your name")
+# myColour = input("plz let me know your fav colour")
+# print(myName + "like" + myColour)
+
+# birth_year = int(input("Birth Year: "))
+# age = 2023 - birth_year 
+# print(age)
+
+# weight_in_pounds = float(input("Plz enter your weight in pounds"))
+# weight_in_kilogram = weight_in_pounds/0.45
+# print("your weight in kilogram is:", weight_in_kilogram)
+
+first = "John"
+last = "Smith"
+message = f'{first} [{last}] is a coder'
+print(message)
